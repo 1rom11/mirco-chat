52767e3883ca5263b46293586e8dfd8c653c26bb
1rom11dev <76579908+1rom11@users.noreply.github.com>
2021-10-28 19:45:56 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
+# set channel
+channel = 3
+radio.set_group(channel)
+
+basic.show_icon(IconNames.YES)
+basic.pause(200)
+
+basic.clear_screen()
+
+# ALL CODE IS MADE IN PXT / MAKECODE
+# Start of Mirco:Chat = 2021/10 by 1rom11:dev (owner of code)
+# send_number 0 / dot
+
+# Send dot
+def on_button_pressed_a():
+    radio.send_number(0)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+#-1
+def on_pin_pressed_p2():
+    global channel
+    channel += -1
+    basic.show_string("" + str(channel))
+    basic.clear_screen()
+    radio.set_group(channel)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
+
+# send_number 1 / line
+
+def on_button_pressed_b():
+    radio.send_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+# +1 Channel
+
+def on_pin_pressed_p1():
+    global channel
+    channel += 1
+    basic.show_string("" + str(channel))
+    basic.clear_screen()
+    radio.set_group(channel)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
 # Receive message in leds / morse code (decoder)
 
 def on_received_number(receivedNumber):
@@ -20,39 +63,3 @@ def on_received_number(receivedNumber):
         """)
         music.play_tone(349, music.beat(BeatFraction.WHOLE))
 radio.on_received_number(on_received_number)
-
-# send_number 0 / dot
-
-def on_button_pressed_a():
-    radio.send_number(0)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_pin_pressed_p2():
-    global channel
-    channel += -1
-    basic.show_string("" + str(channel))
-    basic.clear_screen()
-    radio.set_group(channel)
-input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
-
-# send_number 1 / line
-
-def on_button_pressed_b():
-    radio.send_number(1)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_pin_pressed_p1():
-    global channel
-    channel += 1
-    basic.show_string("" + str(channel))
-    basic.clear_screen()
-    radio.set_group(channel)
-input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
-
-channel = 0
-# set channel
-channel = 3
-radio.set_group(channel)
-
-# ALL CODE IS MADE IN PXT / MAKECODE
-# Start of Mirco:Chat = 2021/10 by 1rom11:dev (owner of code)
--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+//  set channel
+let channel = 3
+radio.setGroup(channel)
+basic.showIcon(IconNames.Yes)
+basic.pause(200)
+basic.clearScreen()
+//  ALL CODE IS MADE IN PXT / MAKECODE
+//  Start of Mirco:Chat = 2021/10 by 1rom11:dev (owner of code)
+//  send_number 0 / dot
+//  Send dot
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    radio.sendNumber(0)
+})
+// -1
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
+    channel += -1
+    basic.showString("" + ("" + channel))
+    basic.clearScreen()
+    radio.setGroup(channel)
+})
+//  send_number 1 / line
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    radio.sendNumber(1)
+})
+//  +1 Channel
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
+    channel += 1
+    basic.showString("" + ("" + channel))
+    basic.clearScreen()
+    radio.setGroup(channel)
+})
 //  Receive message in leds / morse code (decoder)
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     if (receivedNumber == 0) {
@@ -21,29 +54,3 @@ radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     }
     
 })
-//  send_number 0 / dot
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendNumber(0)
-})
-input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
-    
-    channel += -1
-    basic.showString("" + ("" + channel))
-    basic.clearScreen()
-    radio.setGroup(channel)
-})
-//  send_number 1 / line
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    radio.sendNumber(1)
-})
-input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
-    
-    channel += 1
-    basic.showString("" + ("" + channel))
-    basic.clearScreen()
-    radio.setGroup(channel)
-})
-let channel = 0
-//  set channel
-channel = 3
-radio.setGroup(channel)
