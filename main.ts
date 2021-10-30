835e5c1ce7046f80eff740771d914331357e988a
1rom11dev <76579908+1rom11@users.noreply.github.com>
2021-10-30 16:05:03 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -34,6 +34,7 @@ input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
     radio.setGroup(channel)
 })
 //  Receive message in leds / morse code (decoder)
+// dez nutes
 radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
     if (receivedNumber == 0) {
         basic.showLeds(`
